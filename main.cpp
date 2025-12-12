88f8f9c68f6d461eb237c8e0d25be0f25eea3ee9
Aditya Gupta <adityagupta2087@gmail.com>
2025-12-13 03:58:51 +0530

    started model: linear regression

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include<iostream> 
+
+int main() 
+{
+    std::cout << "this is working\n";
+    return 0;
+}
---
+++ models/linear_regression.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+namespace models { 
+    class LinearRegression2D { 
+        public: 
+            LinearRegression2D(double x[], double y[]) { 
+
+            }
+    };
+};
